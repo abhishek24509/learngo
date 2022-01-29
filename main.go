7d18b843858695b0c5410f84f88be716a6ba658a
Abhishek Saxena <abhisheksaxena@jarvis.local>
2022-01-29 23:11:34 +0530

    added function

--- main.go
+++ main.go
@@ -13,4 +13,14 @@ func main() {
 
 	helloFunc()
 
+	helloFunc3 := func() {
+		fmt.Println("hello abhishek welcome to go world helloFunc3")
+	}
+
+	var helloFunc2 *func()
+
+	helloFunc2 = &helloFunc3
+
+	(*helloFunc2)()
+
 }
