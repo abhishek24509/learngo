5efe149f0b422076f26f1f8fee08514529193fd3
Abhishek Saxena <abhisheksaxena@jarvis.local>
2022-01-29 23:01:47 +0530

    added main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello gophers !!!")
+
+	helloFunc := func() {
+		fmt.Println("hello abhishek welcome to go world")
+	}
+
+	helloFunc()
+
+}
